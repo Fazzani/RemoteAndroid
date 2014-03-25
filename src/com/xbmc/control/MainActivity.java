c1597133fbcb17466a4f0491b55c2307c22b2bf2
Fazzani <tunisienheni@outlook.com>
2014-03-25 01:43:29 +0100

    HFA: Automatique Screen Off/On

--- src/com/xbmc/control/MainActivity.java
+++ src/com/xbmc/control/MainActivity.java
@@ -15,12 +15,19 @@ import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.media.MediaPlayer;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.PowerManager;
+import android.os.SystemClock;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
@@ -49,13 +56,23 @@ import android.webkit.WebViewClient;
 
 /**
  * @author 922261
- *
+ * 
  */
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements SensorEventListener {
+
 	private static final String URL_WEBAPP = "http://henifezzeni.hd.free.fr:8080/Remote/index.php";
 	private WebView mWebView;
+	/** * Le sensor manager */
+	SensorManager sensorManager;
+	PowerManager _powerManager;
+	/** * l'accéléromètre */
+	Sensor orientaton;
+	private PowerManager.WakeLock wakeLock;
+	private int field = 0x00000020;
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see android.app.Activity#onCreate(android.os.Bundle)
 	 */
 	@Override
@@ -63,6 +80,11 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 
 		final Activity activity = this;
+		sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+		// Instancier l'accéléromètre
+		orientaton = sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION);
+		_powerManager = (PowerManager) getSystemService(Context.POWER_SERVICE);
+		wakeLock = _powerManager.newWakeLock(field, getLocalClassName());
 		super.onCreate(savedInstanceState);
 		CookieSyncManager.createInstance(activity);
 		CookieSyncManager.getInstance().startSync();
@@ -70,11 +92,11 @@ public class MainActivity extends Activity {
 		CookieManager.getInstance().removeExpiredCookie();
 		Window w = activity.getWindow();
 		w.requestFeature(Window.FEATURE_NO_TITLE);
-		// w.addFlags(LayoutParams.FLAG_KEEP_SCREEN_ON);
+		w.addFlags(LayoutParams.FLAG_KEEP_SCREEN_ON);
 		// in Activity's onCreate() for instance
 		w.addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN);
 		// w.requestFeature(Window.FEATURE_PROGRESS);
-		//setContentView(R.layout.activity_main);
+		// setContentView(R.layout.activity_main);
 		startWebView(URL_WEBAPP);
 		setContentView(mWebView);
 		if (isOnline()) {
@@ -97,7 +119,7 @@ public class MainActivity extends Activity {
 	 * @param url
 	 */
 	private void startWebView(String url) {
-		
+
 		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
 			if (0 != (getApplicationInfo().flags &= ApplicationInfo.FLAG_DEBUGGABLE)) {
 				WebView.setWebContentsDebuggingEnabled(true);
@@ -113,7 +135,7 @@ public class MainActivity extends Activity {
 		webSettings.setDomStorageEnabled(true);
 		webSettings.setLoadWithOverviewMode(true);
 		webSettings.setUseWideViewPort(true);
-		//webSettings.setCacheMode(WebSettings.LOAD_NO_CACHE);
+		// webSettings.setCacheMode(WebSettings.LOAD_NO_CACHE);
 		// webSettings.setSupportMultipleWindows(true);
 		mWebView.setHorizontalScrollBarEnabled(false);
 		mWebView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
@@ -126,6 +148,19 @@ public class MainActivity extends Activity {
 		}
 
 		mWebView.setWebViewClient(new ControlWebViewClient(this));
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		sensorManager.registerListener(this, orientaton,
+				SensorManager.SENSOR_DELAY_UI);
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		sensorManager.unregisterListener(this, orientaton);
 	}
 
 	@Override
@@ -149,4 +184,41 @@ public class MainActivity extends Activity {
 
 		return false;
 	}
+
+	@Override
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onSensorChanged(SensorEvent event) {
+		if (event.sensor.getType() == Sensor.TYPE_ORIENTATION) {
+			//float azimuth_angle = event.values[0];
+			float pitch_angle = event.values[1];
+			//float roll_angle = event.values[2];
+			_powerManager = (PowerManager) getSystemService(Context.POWER_SERVICE);
+			if ((pitch_angle >= 150 || pitch_angle <= -150)
+					&& _powerManager.isScreenOn()) {
+				screenOnOff(true);
+			} else {
+				screenOnOff(false);
+			}
+
+		}
+	}
+
+	public void screenOnOff(Boolean screenOff) {
+		try {
+			if (!wakeLock.isHeld() && screenOff) {
+				wakeLock.acquire();
+			}
+
+			else if (!screenOff && wakeLock.isHeld())
+				wakeLock.release();
+		} catch (Exception e) {
+			// do something useful
+		}
+	}
+
 }
